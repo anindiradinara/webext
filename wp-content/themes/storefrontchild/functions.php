0979d8ac58af8a18979e3aee007f9cbd1a075bfe
Anindira <anindiradinara@gmail.com>
2021-06-20 21:47:44 +0700

    Remove Sale label #3 attempt

--- wp-content/themes/storefrontchild/functions.php
+++ wp-content/themes/storefrontchild/functions.php
@@ -31,8 +31,13 @@ if ( ! function_exists( 'storefront_credit' ) ) {
 	}
 }
 
-remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 ); 
-remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
+function my_remove_sale_labels() {
+    remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10);
+    remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10);
+    remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 6);
+}
+add_action('init', 'my_remove_sale_labels');
+
 function anin_sale_badge( $html, $post, $product ) {
     /*
     if( $product->is_type('variable')) {
